615e57ca3690eca74caf75490a4554e5d30e2c12
米老朱 <laozhu.me@gmail.com>
2018-12-19 00:35:53 +0800

    Add stylelint-scss plugin

---
+++ scss.js
@@ -0,0 +1,18 @@
+const rules = require('./rules/base');
+const rulesOrder = require('./rules/order');
+const unsupportedBrowser = require('./rules/unsupported-browser');
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-prettier',
+  ],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-no-unsupported-browser-features',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
+  rules: { ...rules, ...rulesOrder, ...unsupportedBrowser },
+};
